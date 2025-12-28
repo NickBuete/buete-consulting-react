3eadc06b5d85d105bc230b82935b31b0ce5c6b1e
Nicholas Buete <nbuete01@gmail.com>
2025-12-28 12:12:12 +1100

    feat: remove unused Link import from ContactPage component

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -4,7 +4,6 @@
  */
 
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { CTASection, ContactForm } from '../../components/ui';
 import { SectionHeader } from '../../components/ui/SectionHeader';
 import { Card, CardHeader, CardTitle, CardContent } from '../../components/ui/Card';
